6abe1637aa67e25767e217bbde29af8a5d947dfc
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-08-14 18:26:03 +0300

    add splash screen

---
+++ app/src/main/java/com/example/mealmate/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.mealmate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() -> {
+            Intent intent= new Intent(this, OnBoardingActivity.class);
+            startActivity(intent);
+            finish();
+        }, 8050);
+
+
+    }
+}
