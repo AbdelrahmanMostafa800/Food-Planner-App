6272e9ee7a6aaf8f460991d211b687b5a53e0776
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-08-15 06:37:56 +0300

    add signup fragment

--- app/src/main/java/com/example/mealmate/LoginFragment.java
+++ app/src/main/java/com/example/mealmate/LoginFragment.java
@@ -2,11 +2,16 @@ package com.example.mealmate;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
 
@@ -24,5 +29,18 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView createAccount=view.findViewById(R.id.createAccount);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_loginFragment_to_signUpFragment);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mealmate/SplashScreenActivity.java
+++ app/src/main/java/com/example/mealmate/SplashScreenActivity.java
@@ -13,7 +13,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(() -> {
             Intent intent= new Intent(this, OnBoardingActivity.class);
             startActivity(intent);
